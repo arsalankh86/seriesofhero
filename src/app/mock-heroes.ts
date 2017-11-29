0dbc63ee4337d14d2f3cfa1a0544e02435c0cc2e
arsalankh86 <softwares.coders@gmail.com>
2017-11-30 03:48:40 +0500

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero}  from './hero';
+
+export const HEROES:Hero[] = [
+    {    id:1, name:"hanse"},
+    {    id:2, name:"corne"},
+    {    id:3, name:"makro"},
+    {    id:4, name:"hasef"},
+    {    id:5, name:"deny"},
+    {    id:6, name:"sago"},
+    {    id:7, name:"remo"},
+    {    id:8, name:"jemy"},
+    {    id:9, name:"gill"}
+]
